322f81a0def241d2f0a761e14d62b2b360291263
Ivy Loi <iloi05@csu.fullerton.edu>
2023-12-04 14:20:28 -0800

    Fixed all errors, first push.

--- part-1/pizzas.cc
+++ part-1/pizzas.cc
@@ -1,9 +1,12 @@
-// TODO add a header
+// Ivy Loi
+// iloi05@csu.fullerton.edu
+// @iloi05
+// Partners: @Wen-qqi
+
+#include "pizzas.h"
 
 #include <fstream>
 #include <iostream>
-
-#include "pizzas.h"
 
 std::vector<std::vector<std::string>> ReadCSV(const std::string& csv_filename,
                                               int columns) {
@@ -31,56 +34,47 @@ std::vector<std::vector<std::string>> ReadCSV(const std::string& csv_filename,
   return table;
 }
 
-// TODO: complete the definition of this constructor.
+// complete the definition of this constructor.
 // Since this is a constructor, it should use a member initialization
 // list to initialize data members.
 // When you are done, delete this comment.
-Pizza::Pizza(std::string pizza_id, double price, int calories) {}
+Pizza::Pizza(std::string pizza_id, double price, int calories)
+    : pizza_id_(pizza_id), price_(price), calories_(calories) {}
 
-// TODO: complete the definition of this constructor.
+// complete the definition of this constructor.
 // Since this is a constructor, it should use a member initialization list
 // to initialize data members.
 // When you are done, delete this comment.
-Pizza::Pizza() {}
+Pizza::Pizza() : price_(0.0), calories_(0) {}
 
-// TODO: write statements to implement this function, and delete
+// write statements to implement this function, and delete
 // this comment.
 // Hint: This is a simple accessor function. The function definition only
 // needs to be a single statement.
-std::string Pizza::PizzaID() const {
-  // TODO: delete the return statement below
-  return "";
-}
+std::string Pizza::PizzaID() const { return pizza_id_; }
 
-// TODO: write statements to implement this function, and delete
+// write statements to implement this function, and delete
 // this comment.
 // Hint: This is a simple accessor function. The function definition
 // only needs to be a single statement.
-double Pizza::Price() const {
-  // TODO: delete the return statement below
-  return 0.0;
-}
+double Pizza::Price() const { return price_; }
 
-// TODO: write statements to implement this function, and delete
+// write statements to implement this function, and delete
 // this comment.
 // Hint: This is a simple accessor function. The function definition
 // only needs to be a single statement.
-int Pizza::Calories() const {
-  // TODO: delete the return statement below
-  return 0;
-}
+int Pizza::Calories() const { return calories_; }
 
-// TODO: write statements to implement this function, and delete
+// write statements to implement this function, and delete
 // this comment.
 // Hint: the price-per-2000 calories is the ratio between the
 // price of that pizza and its number of calories it has,
 // multiplied by 2000.
 double Pizza::PricePer2000Calories() const {
-  // TODO: delete the return statement below
-  return 0;
+  return (price_ / calories_) * 2000.0;
 }
 
-// TODO: write statements to implement this function, and delete
+// write statements to implement this function, and delete
 // this comment.
 std::vector<Pizza> ReadPizzas(const std::string& csv_filename) {
   // Create an empty vector of Pizzas
@@ -96,8 +90,21 @@ std::vector<Pizza> ReadPizzas(const std::string& csv_filename) {
   // - Convert the string at column 4 into an integer
   // - Create an object of class Pizza with the data from columns 1, 3, and 4
   // - Add the object with push_back to the vector of objects of class Pizza
-  // TODO: delete the return statement below
-  return std::vector<Pizza>{};
+  // delete the return statement below
   // and replace it with one that actually works, which is the
   // vector of pizzas
+  std::vector<Pizza> many_pizzas;
+  auto csv_data = ReadCSV(csv_filename, 5);
+  for (size_t j = 1; j < csv_data.size(); ++j) {
+    try {
+      std::string pizza_id = csv_data[j][1];
+      double price = std::stod(csv_data[j][3]);
+      int calories = std::stoi(csv_data[j][4]);
+
+      many_pizzas.emplace_back(Pizza(pizza_id, price, calories));
+    } catch (const std::invalid_argument& e) {
+    }
+  }
+
+  return many_pizzas;
 }
